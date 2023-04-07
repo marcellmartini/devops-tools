fef67ec9c49db589e8a5a50785c0853c4d540f9f
Marcell Martini <marcellmartini@gmail.com>
2023-04-07 19:57:27 -0400

    --wip-- [skip ci]

---
+++ gitops/argocd/terraform/2-istio.tf
@@ -0,0 +1,65 @@
+resource "helm_release" "istio_base" {
+  name = "my-istio-base"
+
+  repository       = "https://istio-release.storage.googleapis.com/charts"
+  chart            = "base"
+  namespace        = kubernetes_namespace.istio_system.metadata[0].name
+  create_namespace = false
+  version          = "1.17.2"
+
+  set {
+    name  = "defaultRevision"
+    value = ""
+  }
+}
+
+resource "helm_release" "istiod" {
+  name = "my-istio-d"
+
+  repository       = "https://istio-release.storage.googleapis.com/charts"
+  chart            = "istiod"
+  namespace        = kubernetes_namespace.istio_system.metadata[0].name
+  create_namespace = false
+  version          = "1.17.2"
+
+  depends_on = [
+    helm_release.istio_base
+  ]
+}
+
+resource "helm_release" "istio_gateway" {
+  name = "ingressgateway"
+
+  repository       = "https://istio-release.storage.googleapis.com/charts"
+  chart            = "gateway"
+  namespace        = kubernetes_namespace.istio_ingress.metadata[0].name
+  create_namespace = false
+  version          = "1.17.2"
+
+  depends_on = [
+    helm_release.istiod
+  ]
+}
+
+resource "kubernetes_namespace" "istio_system" {
+  metadata {
+    name = "istio-system"
+  }
+}
+
+resource "kubernetes_namespace" "istio_ingress" {
+  metadata {
+    name = "istio-ingress"
+  }
+}
+
+resource "kubernetes_labels" "default_namespace" {
+  api_version = "v1"
+  kind        = "Namespace"
+  metadata {
+    name = "default"
+  }
+  labels = {
+    "istio-injection" = "enabled"
+  }
+}
