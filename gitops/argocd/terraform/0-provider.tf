4ebd60ebfee8e224f5bbf5ec1aaf2eb5726aee03
Marcell Martini <marcellmartini@gmail.com>
2023-04-07 22:36:02 -0400

    --wip-- [skip ci]

--- gitops/argocd/terraform/0-provider.tf
+++ gitops/argocd/terraform/0-provider.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.19.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.9.0"
+    }
+  }
+}
+
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
