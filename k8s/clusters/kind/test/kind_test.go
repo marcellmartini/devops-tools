47e153dd6f0653c1dce4b77a3b5924b46c828f62
Marcell Martini <marcellmartini@gmail.com>
2023-04-19 11:17:37 -0400

    feat: Add test to the kind config file
    * Adding terratest test to validate the kind config file.

---
+++ k8s/clusters/kind/test/kind_test.go
@@ -0,0 +1,76 @@
+package test
+
+import (
+	"io/ioutil"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/gruntwork-io/terratest/modules/shell"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v2"
+	"k8s.io/apimachinery/pkg/util/rand"
+)
+
+type kindYaml struct {
+	APIVersion string `yaml:"apiVersion"`
+	Kind       string `yaml:"kind"`
+	Nodes      []struct {
+		Role string `yaml:"role"`
+	} `yaml:"nodes"`
+}
+
+func (ky *kindYaml) getYaml(path string) *kindYaml {
+	yf, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Printf("yamlFile.Get err #%v ", err)
+	}
+
+	err = yaml.Unmarshal(yf, ky)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
+	return ky
+}
+
+func TestKindClusterCriation(t *testing.T) {
+	t.Parallel()
+
+	clusterName := "test-" + rand.String(8)
+	kindConfigFile := "../kind_cluster.yml"
+	clusterCreate := shell.Command{
+		Command: "kind",
+		Args: []string{
+			"create",
+			"cluster",
+			"--config",
+			kindConfigFile,
+			"--name",
+			clusterName,
+		},
+	}
+	clusterDelete := shell.Command{
+		Command: "kind",
+		Args: []string{
+			"delete",
+			"cluster",
+			"--name",
+			clusterName,
+		},
+	}
+
+	out := shell.RunCommandAndGetOutput(t, clusterCreate)
+	defer shell.RunCommand(t, clusterDelete)
+
+	assert.Contains(t, out, "You can now use your cluster with", "kubectl cluster-info --context kind-"+clusterName)
+
+	ky := kindYaml{}
+	ky.getYaml(kindConfigFile)
+
+	options := k8s.NewKubectlOptions("", "", "default")
+	k8s.WaitUntilAllNodesReady(t, options, 30, 3*time.Second)
+	assert.True(t, k8s.AreAllNodesReady(t, options))
+	assert.EqualValues(t, len(ky.Nodes), len(k8s.GetNodes(t, options)))
+}
